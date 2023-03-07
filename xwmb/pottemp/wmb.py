2d7a1dde269d933a1acc991e479f2f3d27608d11
Henri Drake <henrifdrake@gmail.com>
2023-03-07 15:22:14 -0500

    Added generic plotting function to summarize WMBs
    - Three panels show (using Bailey conventions): R1 residual discription errors, R2 residual numerical mixing errors, and the full water mass budget (WMB)

--- xwmb/pottemp/wmb.py
+++ xwmb/pottemp/wmb.py
@@ -2,6 +2,8 @@ import xarray as xr
 import numpy as np
 import warnings
 
+import matplotlib.pyplot as plt
+
 from .wmt import *
 from .dMdt import *
 from .psi import *
@@ -46,3 +48,43 @@ def transform_to_theta(ds, grid, theta_i_bins, theta_l_levs):
         ds_theta = ds_theta.assign_coords({"thetao_l": theta_l_levs})
         
     return ds_theta
+
+def plot_wmb(wmb, ylim=[-2, 22], rho0=1035.):
+    plt.figure(figsize=(12,5))
+
+    plt.subplot(1,3,1)
+    (-wmb.dMdt.mean('time')  / rho0*1e-6 ).plot(y="thetao_i", label=r"$-$d$M/$d$t$, from diff of $h(\Theta)$ snapshots")
+    ((wmb.G_tend).mean('time') / rho0*1e-6 ).plot(y="thetao_i", label=r"$G_{tend}$ from $\dot{\Theta}$")
+    plt.title("")
+    plt.legend(fontsize=10, loc='upper right')
+    plt.grid(True, alpha=0.15)
+    plt.ylabel("Potential temperature [$\degree$C]")
+    plt.xlabel("Watermass volume tendency [Sv]")
+    plt.ylim(ylim)
+
+    plt.subplot(1,3,2)
+    (-wmb.Psi.mean('time')  / rho0*1e-6 ).plot(y="thetao_i", label="$-\Psi(\Theta)$ from $\mathbf{u}(\Theta)$")
+    ((-wmb.G_adv).mean('time') / rho0*1e-6 ).plot(y="thetao_i", label="$-G_{adv}$ from $\dot{\Theta}$")
+    plt.title("")
+    plt.legend(fontsize=10, loc='upper right')
+    plt.grid(True, alpha=0.15)
+    plt.ylabel("Potential temperature [$\degree$C]")
+    plt.xlabel("Watermass volume tendency [Sv]")
+    plt.ylim(ylim)
+    
+    plt.subplot(1,3,3)
+    plt.fill_betweenx(
+        wmb.thetao_i,
+        wmb.G_dia.mean('time')/rho0*1e-6, -(wmb.Psi+wmb.dMdt).mean('time')/rho0*1e-6,
+        alpha=0.25, color="C4", label=r"$-G_{num}$"
+    )
+    ((-(wmb.dMdt + wmb.Psi))/rho0*1e-6).mean('time').plot(y="thetao_i", label=r"$-(\Psi + $d$M/$d$t$)")
+    ((wmb.G_tend - wmb.G_adv)/rho0*1e-6).mean('time').plot(y="thetao_i", label=r"$G_{tend} - G_{adv}$")
+    ((wmb.G_dia) / rho0*1e-6).mean('time').plot(y="thetao_i", label=r"$G_{dia}$", linestyle="--", alpha=0.8, lw=1.)
+    plt.title("")
+    plt.legend(fontsize=10, loc='upper right')
+    plt.grid(True, alpha=0.15)
+    plt.ylabel("Potential temperature [$\degree$C]")
+    plt.xlabel("Watermass volume tendency [Sv]")
+    plt.ylim(ylim)
+    plt.tight_layout()
