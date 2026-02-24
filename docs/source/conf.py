0074c449fe31968709298ff53a6a027ba2ee8236
Henri Drake <hfdrake@uci.edu>
2026-02-24 06:13:29 -0800

    Exclude previous build outputs so as not to confuse later builds
    Should fix this warning: https://app.readthedocs.org/projects/xwmb/builds/31531545/

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,12 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    "_build",
+    "**/_build",
+    "**/.ipynb_checkpoints",
+    ".DS_Store",
+]
 nbsphinx_execute = "never"
 
 # -- Copy notebooks from repo root/examples into docs/source/examples --------
